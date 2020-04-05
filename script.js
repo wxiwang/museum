80f8e5598c2bdd10f4142e4d05fdf9e315d3b161
Glitch (museum-wxiwang) <none>
2020-04-05 18:16:50 +0000

    Add photo sliding, clock and a stickt footer.

--- script.js
+++ script.js
@@ -2,4 +2,40 @@
     to your site with Javascript */
 
 // prints "hi" in the browser's dev tools console
-console.log("hi");
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+
+var ct = document.getElementsByClassName("counter");
+let ret=ct[0].innerHTML;
+ret=3623037;
+setInterval(function(){
+            ret--;
+            ct[0].innerHTML=String(ret).replace(/(\d)(?=(\d{3})+$)/g, "$1,");;
+        },1000)
+
